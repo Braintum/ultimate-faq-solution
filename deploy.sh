6b2fe7ce9b7b3c17f28c879bc1c4baec99c3554d
Mahedi Hasan Noman <mahedihasannoman@gmail.com>
2021-05-20 01:28:09 +0600

    vendor renamed to third-party

--- deploy.sh
+++ deploy.sh
@@ -1,245 +1,274 @@
 #! /bin/bash
-# ----- START EDITING HERE -----
-PLUGINSLUG="ultimate-faq-solution"
-SVNUSER="braintum"
-# ----- STOP EDITING HERE -----
+# See https://github.com/GaryJones/wordpress-plugin-svn-deploy for instructions and credits.
+#
+# Steps to deploying:
+#
+#  1. Ask for plugin slug.
+#  2. Ask for local plugin directory.
+#  3. Check local plugin directory exists.
+#  4. Ask for local SVN assets directory.
+#  5. Check local SVN assets directory exists.
+#  6. Ask for main plugin file name.
+#  7. Check main plugin file exists.
+#  8. Check readme.txt version matches main plugin file version.
+#  9. Ask for temporary SVN path.
+# 10. Ask for remote SVN repo.
+# 11. Ask for SVN username.
+# 12. Ask if input is correct, and give chance to abort.
+# 13. Check if Git tag exists for version number (must match exactly).
+# 14. Checkout SVN repo.
+# 15. Set to SVN ignore some GitHub-related files.
+# 16. Export HEAD of master from git to the trunk of SVN.
+# 17. Initialise and update and git submodules.
+# 18. Move /trunk/assets up to /assets.
+# 19. Move into /trunk, and SVN commit.
+# 20. Move into /assets, and SVN commit.
+# 21. Copy /trunk into /tags/{version}, and SVN commit.
+# 22. Delete temporary local SVN checkout.
 
-# Exit if any command fails.
-set -e
+echo
+echo "WordPress Plugin SVN Deploy v4.1.0"
+echo
+echo "Let's collect some information first. There are six questions."
+echo
+echo "Default values are in brackets - just hit enter to accept them."
+echo
 
-# Enable nicer messaging for build status.
-BLUE_BOLD='\033[1;34m'
-GREEN_BOLD='\033[1;32m'
-RED_BOLD='\033[1;31m'
-YELLOW_BOLD='\033[1;33m'
-COLOR_RESET='\033[0m'
-error() {
-	echo -e "${RED_BOLD}$1${COLOR_RESET}"
-}
-status() {
-	echo -e "${BLUE_BOLD}$1${COLOR_RESET}"
-}
-success() {
-	echo -e "${GREEN_BOLD}$1${COLOR_RESET}"
-}
-warning() {
-	echo -e "${YELLOW_BOLD}$1${COLOR_RESET}"
-}
+# Get some user input
+# Can't use the -i flag for read, since that doesn't work for bash 3
+printf "Q1. WordPress Repo Plugin Slug e.g. my-awesome-plugin: "
+read -e PLUGINSLUG
+echo
 
-# ASK INFO
-status "--------------------------------------------"
-status "      Github to WordPress.org RELEASER      "
-status "--------------------------------------------"
-read -p "TAG AND RELEASE VERSION: " VERSION
-status "--------------------------------------------"
-status ""
-status "Before continuing, confirm that you have done the following :)"
-status ""
-read -p " - Set version in main file header to "${VERSION}"?"
-read -p " - Set version in main file PHP to "${VERSION}"?"
-read -p " - Set version in the package.json to "${VERSION}"?"
-read -p " - Included update file update-"${VERSION}".php?"
-read -p " - Added update file in array for "${VERSION}".php?"
-read -p " - Added a changelog for "${VERSION}"?"
-read -p " - Updated the POT file?"
-read -p " - Committed all changes up to GITHUB?"
-status ""
-read -p "PRESS [ENTER] TO BEGIN RELEASING "${VERSION}
-
+# Set up some default values. Feel free to change these in your own script
 CURRENTDIR=$(pwd)
-SVNPATH="/tmp/$PLUGINSLUG"
-SVNURL="https://plugins.svn.wordpress.org/$PLUGINSLUG"
-PLUGINDIR="$CURRENTDIR"
-MAINFILE="init.php"
-ASSETSDIR=".wordpress-org"
+default_svnpath="/tmp/$PLUGINSLUG"
+default_svnurl="https://plugins.svn.wordpress.org/$PLUGINSLUG"
+default_svnuser="GaryJ"
+default_plugindir="$CURRENTDIR/$PLUGINSLUG"
+default_mainfile="$PLUGINSLUG.php"
+default_assetsdir=".wordpress-org"
+
+echo "Q2. Your local plugin root directory (the Git repo)."
+printf "($default_plugindir): "
+read -e input
+input="${input%/}"                       # Strip trailing slash
+PLUGINDIR="${input:-$default_plugindir}" # Populate with default if empty
+echo
+
+# Check directory exists.
+if [ ! -d "$PLUGINDIR" ]; then
+	echo "Directory $PLUGINDIR not found. Aborting."
+	exit 1
+fi
+
+echo "Q3. Your local repository directory for SVN assets."
+printf "($default_assetsdir): "
+read -e input
+ASSETSDIR="${input:-$default_assetsdir}" # Populate with default if empty
+echo
 
 # Check if SVN assets directory exists.
 if [ ! -d "$PLUGINDIR/$ASSETSDIR" ]; then
-	status "SVN assets directory $PLUGINDIR/$ASSETSDIR not found."
-	warning "This is not fatal but you may not have intended results."
+	echo "SVN assets directory $PLUGINDIR/$ASSETSDIR not found."
+	echo "This is not fatal but you may not have intended results."
+	echo
 fi
+
+printf "Q4. Name of the main plugin file ($default_mainfile): "
+read -e input
+MAINFILE="${input:-$default_mainfile}" # Populate with default if empty
+echo
+
+# Check main plugin file exists.
+if [ ! -f "$PLUGINDIR/$MAINFILE" ]; then
+	echo "Plugin file $PLUGINDIR/$MAINFILE not found. Aborting."
+	exit 1
+fi
+
+echo "Checking version in main plugin file matches version in readme.txt file..."
+echo
+
+# Check version in readme.txt is the same as plugin file after translating both to Unix line breaks to work around grep's failure to identify Mac line breaks
+PLUGINVERSION=$(grep -i "Version:" $PLUGINDIR/$MAINFILE | awk -F' ' '{print $NF}' | tr -d '\r')
+echo "$MAINFILE version: $PLUGINVERSION"
+READMEVERSION=$(grep -i "Stable tag:" $PLUGINDIR/readme.txt | awk -F' ' '{print $NF}' | tr -d '\r')
+echo "readme.txt version: $READMEVERSION"
+
+if [ "$READMEVERSION" = "trunk" ]; then
+	echo "Version in readme.txt & $MAINFILE don't match, but Stable tag is trunk. Let's continue..."
+elif [ "$PLUGINVERSION" != "$READMEVERSION" ]; then
+	echo "Version in readme.txt & $MAINFILE don't match. Exiting...."
+	exit 1
+elif [ "$PLUGINVERSION" = "$READMEVERSION" ]; then
+	echo "Versions match in readme.txt and $MAINFILE. Let's continue..."
+fi
+
+echo
+
+echo "Q5. Path to a local directory where a temporary SVN checkout can be made."
+printf "Don't add trunk ($default_svnpath): "
+read -e input
+input="${input%/}"                   # Strip trailing slash
+SVNPATH="${input:-$default_svnpath}" # Populate with default if empty
+echo
+
+echo "Q6. Remote SVN repo on WordPress.org."
+printf "($default_svnurl): "
+read -e input
+input="${input%/}"                 # Strip trailing slash
+SVNURL="${input:-$default_svnurl}" # Populate with default if empty
+echo
+
+printf "Q7. Your WordPress repo SVN username ($default_svnuser): "
+read -e input
+SVNUSER="${input:-$default_svnuser}" # Populate with default if empty
+echo
+
+echo "That's all of the data collected."
+echo
+echo "Slug: $PLUGINSLUG"
+echo "Plugin directory: $PLUGINDIR"
+echo "Main file: $MAINFILE"
+echo "Temp checkout path: $SVNPATH"
+echo "Remote SVN repo: $SVNURL"
+echo "SVN username: $SVNUSER"
+echo
+
+printf "OK to proceed (Y|n)? "
+read -e input
+PROCEED="${input:-y}"
+echo
+
+# Allow user cancellation
+if [ $(echo "$PROCEED" | tr [:upper:] [:lower:]) != "y" ]; then
+	echo "Aborting..."
+	exit 1
+fi
+
+# Let's begin...
+echo ".........................................."
+echo
+echo "Preparing to deploy WordPress plugin"
+echo
+echo ".........................................."
+echo
+
+echo
+
+echo "Changing to $PLUGINDIR"
+cd $PLUGINDIR
 
 # Check for git tag (may need to allow for leading "v"?)
-# if git show-ref --tags --quiet --verify -- "refs/tags/$VERSION"
-if git show-ref --tags --quiet --verify -- "refs/tags/v$VERSION"; then
-	status "Git tag $VERSION does exist. Let's continue..."
+# if git show-ref --tags --quiet --verify -- "refs/tags/$PLUGINVERSION"
+if git show-ref --tags --quiet --verify -- "refs/tags/$PLUGINVERSION"; then
+	echo "Git tag $PLUGINVERSION does exist. Let's continue..."
 else
-	error "$VERSION does not exist as a git tag. Aborting."
+	echo "$PLUGINVERSION does not exist as a git tag. Aborting."
 	exit 1
 fi
 
 echo
-success "💃 Time to release plugin 🕺"
-echo
-status "Creating local copy of SVN repo trunk..."
-rm -rf $SVNPATH
+
+echo "Creating local copy of SVN repo trunk..."
 svn checkout $SVNURL $SVNPATH --depth immediates
-svn update --quiet $SVNPATH/assets --set-depth infinity
 svn update --quiet $SVNPATH/trunk --set-depth infinity
-svn update --quiet $SVNPATH/tags/$VERSION --set-depth infinity
+svn update --quiet $SVNPATH/tags/$PLUGINVERSION --set-depth infinity
 
-status ""
-status -p "PRESS [ENTER] TO DEPLOY VERSION "${VERSION}
+echo "Ignoring GitHub specific files"
+# Use local .svnignore if present
+if [ -f ".svnignore" ]; then
+	echo "Using local .svnignore"
+	SVNIGNORE=$(<.svnignore)
+else
+	echo "Using default .svnignore"
+	SVNIGNORE="README.md
+Thumbs.db
+.github
+.git
+.gitattributes
+.gitignore
+composer.lock"
+fi
 
-# UPDATE SVN
-status "Updating SVN"
-svn update $SVNPATH || {
-	echo "Unable to update SVN."
-	exit 1
-}
+svn propset svn:ignore \""$SVNIGNORE"\" "$SVNPATH/trunk/"
 
-status "Replacing trunk"
-rm -Rf $SVNPATH/trunk/*
-
-status "Moving to git "
-cd $PLUGINDIR
-status "Exporting the HEAD of master from git to the trunk of SVN"
+echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
 # If submodule exist, recursively check out their indexes
 if [ -f ".gitmodules" ]; then
-	status "Exporting the HEAD of each submodule from git to the trunk of SVN"
+	echo "Exporting the HEAD of each submodule from git to the trunk of SVN"
 	git submodule init
 	git submodule update
 	git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
 		while read path_key path; do
-			#url_key=$(status $path_key | sed 's/\.path/.url/')
+			#url_key=$(echo $path_key | sed 's/\.path/.url/')
 			#url=$(git config -f .gitmodules --get "$url_key")
 			#git submodule add $url $path
-			status "This is the submodule path: $path"
-			status "The following line is the command to checkout the submodule."
-			status "git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'"
+			echo "This is the submodule path: $path"
+			echo "The following line is the command to checkout the submodule."
+			echo "git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'"
 			git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'
 		done
 fi
-#install composer
-status "Installing PHP dependencies";
-cd $SVNPATH/trunk
-composer install --no-dev
-composer du -o
 
+echo
 
 # Support for the /assets folder on the .org repo, locally this will be /.wordpress-org
-status "Moving assets."
+echo "Moving assets."
 # Make the directory if it doesn't already exist
 mkdir -p $SVNPATH/assets/
 mv $SVNPATH/trunk/.wordpress-org/* $SVNPATH/assets/
 svn add --force $SVNPATH/assets/
 
-# REMOVE UNWANTED FILES & FOLDERS
-cd $SVNPATH
-status "Removing unwanted files"
-rm -Rf trunk/assets/css/*.scss
-rm -Rf trunk/assets/css/metabox/*.scss
-rm -Rf trunk/assets/*.scss
-rm -Rf trunk/bin
-rm -Rf trunk/**/.gitkeep
-rm -Rf trunk/.git
-rm -Rf trunk/.babelrc
-rm -Rf trunk/yarn.lock
-rm -Rf trunk/.github
-rm -Rf trunk/.wordpress-org
-rm -Rf trunk/.svnignore
-rm -Rf trunk/apigen
-rm -Rf trunk/tests
-rm -f trunk/.coveralls.yml
-rm -f trunk/.editorconfig
-rm -f trunk/.gitattributes
-rm -f trunk/.gitignore
-rm -f trunk/.gitmodules
-rm -f trunk/.jscrsrc
-rm -f trunk/.jshintrc
-rm -f trunk/.scrutinizer.yml
-rm -f trunk/.stylelintrc
-rm -f trunk/.travis.yml
-rm -f trunk/apigen.neon
-rm -f trunk/CHANGELOG.txt
-rm -f trunk/CODE_OF_CONDUCT.md
-rm -f trunk/composer.json
-rm -f trunk/composer.lock
-rm -f trunk/CONTRIBUTING.md
-rm -f trunk/docker-compose.yml
-rm -f trunk/Gruntfile.js
-rm -f trunk/package.json
-rm -f trunk/phpcs.xml
-rm -f trunk/phpunit.xml
-rm -f trunk/phpunit.xml.dist
-rm -f trunk/README.md
-rm -f trunk/deploy.sh
-rm -f trunk/package-lock.json
-rm -f trunk/phpcs.xml.dist
-rm -f trunk/tmp
-rm -f trunk/.phpcs.xml.dist
+echo
 
-# DO THE ADD ALL NOT KNOWN FILES UNIX COMMAND
-svn add --force * --auto-props --parents --depth infinity -q
-
+echo "Changing directory to SVN and committing to trunk."
+cd $SVNPATH/trunk/
+# Delete all files that should not now be added.
+# Use $SVNIGNORE for `rm -rf`. Setting propset svn:ignore seems flaky.
+echo "$SVNIGNORE" | awk '{print $0}' | xargs rm -rf
 svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
 # Add all new files that are not set to be ignored
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
+svn commit --username=$SVNUSER -m "Preparing for $PLUGINVERSION release"
 
-# DO SVN COMMIT
-status "Showing SVN status"
-svn status
-
-# PROMPT USER
-echo ""
-printf "CONFIRM TO COMMIT RELEASE (Y|N)? "
-echo ""
-read -e input
-PROCEED="${input:-y}"
 echo
 
-# Allow user cancellation
-if [ $(echo "$PROCEED" | tr [:upper:] [:lower:]) == "y" ]; then
-	status "Pushing..."
-	svn commit --username=$SVNUSER -m "Preparing for $VERSION release"
-else
-	warning "Aboring..."
-fi
+echo "Updating WordPress plugin repo assets and committing."
+cd $SVNPATH/assets/
+# Delete all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
+# Add all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
+svn update --quiet --accept working $SVNPATH/assets/*
+svn resolve --accept working $SVNPATH/assets/*
+svn commit --username=$SVNUSER -m "Updating assets"
 
-# PROMPT USER
-echo ""
-printf "CONFIRM TO UPDATE ASSETS (Y|N)? "
-echo ""
-read -e input
-PROCEED="${input:-y}"
-echo ""
+echo
 
-# Allow user cancellation
-if [ $(echo "$PROCEED" | tr [:upper:] [:lower:]) == "y" ]; then
-	status "Updating WordPress plugin repo assets and committing."
-	cd $SVNPATH/assets/
-	# Delete all new files that are not set to be ignored
-	svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
-	# Add all new files that are not set to be ignored
-	svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
-	svn update --quiet --accept working $SVNPATH/assets/*
-	svn resolve --accept working $SVNPATH/assets/*
-	svn commit --username=$SVNUSER -m "Updating assets"
-else
-	warning "Aboring assets push ..."
-fi
-
-# PROMPT USER
-echo ""
-printf "CONFIRM TO TAG (Y|N)? "
-echo ""
-read -e input
-PROCEED="${input:-y}"
-echo ""
-
-# Allow user cancellation
-if [ $(echo "$PROCEED" | tr [:upper:] [:lower:]) == "y" ]; then
-	status "Creating new SVN tag and committing it."
-	svn cp -m"tagging v$VERSION" https://plugins.svn.wordpress.org/ultimate-faq-solution/trunk https://plugins.svn.wordpress.org/ultimate-faq-solution/tags/$VERSION
-else
-	warning "Aboring tag..."
-fi
-
-status "Removing temporary directory $SVNPATH."
-
+echo "Creating new SVN tag and committing it."
 cd $SVNPATH
+# If current tag not empty then update readme.txt
+if [ -n "$(ls -A tags/$PLUGINVERSION 2>/dev/null)" ]; then
+	echo "Updating readme.txt to tag $PLUGINVERSION"
+	svn delete --force tags/$PLUGINVERSION/readme.txt
+	svn copy trunk/readme.txt tags/$PLUGINVERSION
+fi
+svn copy --quiet trunk/ tags/$PLUGINVERSION/
+# Remove trunk directories from tag directory
+svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/trunk
+svn update --quiet --accept working $SVNPATH/tags/$PLUGINVERSION
+#svn resolve --accept working $SVNPATH/tags/$PLUGINVERSION/*
+cd $SVNPATH/tags/$PLUGINVERSION
+svn commit --username=$SVNUSER -m "Tagging version $PLUGINVERSION"
+
+echo
+
+echo "Removing temporary directory $SVNPATH."
+cd $SVNPATH
+cd ..
 rm -fr $SVNPATH/
 
-success "*** FIN ***"
+echo "*** FIN ***"
--- init.php
+++ init.php
@@ -34,12 +34,12 @@ define( 'UFAQSW__ASSETS_URL', UFAQSW__PLUGIN_URL.'assets/' );
 */
 
 
-if ( file_exists( dirname( __FILE__ ) . '/vendor/cmb2/init.php' ) ) {
-	require_once dirname( __FILE__ ) . '/vendor/cmb2/init.php';
+if ( file_exists( dirname( __FILE__ ) . '/third-party/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/third-party/cmb2/init.php';
 }
 
-if ( file_exists( dirname( __FILE__ ) . '/vendor/cmb2-conditionals/cmb2-conditionals.php' ) ) {
-	require_once dirname( __FILE__ ) . '/vendor/cmb2-conditionals/cmb2-conditionals.php';
+if ( file_exists( dirname( __FILE__ ) . '/third-party/cmb2-conditionals/cmb2-conditionals.php' ) ) {
+	require_once dirname( __FILE__ ) . '/third-party/cmb2-conditionals/cmb2-conditionals.php';
 }
 if (is_admin()){
 	require_once UFAQSW__PLUGIN_DIR . 'admin/class-directory-post-type.php';
