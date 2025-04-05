941be228bb6267522658dbc4fb4d6a7ed8296bdd
mahedihasannoman <mahedihasannoman@gmail.com>
2025-04-06 00:24:09 +0200

    Default template accordion issue fixed

--- src/scripts/default.js
+++ src/scripts/default.js
@@ -3,20 +3,25 @@ jQuery(document).ready(function($){
 	if( jQuery(".ufaqsw_toggle_default .ufaqsw-toggle-title-area-default").hasClass('ufaqsw_active') ){
 		jQuery(".ufaqsw_toggle_default .ufaqsw-toggle-title-area-default.ufaqsw_active").closest('.ufaqsw_toggle_default').find('.ufaqsw-toggle-inner-default').show();
 	}
-	
+
 	//Handle click event
 	jQuery(".ufaqsw_toggle_default .ufaqsw-toggle-title-area-default").on('click', function(){
-		
+
+
 		if(ufaqsw_object_default.behaviour=='accordion'){
+			if ( jQuery(this).hasClass('ufaqsw_active') ) {
+				return;
+			}
+			
 			closeall();
 		}
+
 		if( jQuery(this).hasClass('ufaqsw_active') ){
 			jQuery(this).removeClass("ufaqsw_active").closest('.ufaqsw_toggle_default').find('.ufaqsw-toggle-inner-default').slideUp(200);
 		}
 		else{	jQuery(this).addClass("ufaqsw_active").closest('.ufaqsw_toggle_default').find('.ufaqsw-toggle-inner-default').slideDown(200);
 		}
 		$(this).find('i').toggle();
-
 		
 	});
 	
