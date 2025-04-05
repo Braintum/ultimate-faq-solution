04bc858fdd908cd36c5beb79fd084d1375bc04d5
mahedihasannoman <mahedihasannoman@gmail.com>
2025-04-05 22:55:12 +0200

    block script built

--- block/build/index.asset.php
+++ block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => '5a1c4f63268e7d6c7eee');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '13429890a23c3d2bded3');
