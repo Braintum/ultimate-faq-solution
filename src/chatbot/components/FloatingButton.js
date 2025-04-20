cb040fe87a971b271f50054fce0f79a128d5b60e
mahedihasannoman <mahedihasannoman@gmail.com>
2025-04-20 18:39:34 +0200

    [No-Ticket] FAQ group, list, answer view added

--- src/chatbot/components/FloatingButton.js
+++ src/chatbot/components/FloatingButton.js
@@ -1,23 +1,11 @@
 import React from "react";
+import "../styles/floatingButton.css"; // Import your CSS file for styling
 
 export const FloatingButton = ({ onClick }) => {
 	return (
 		<button
 		onClick={onClick}
-		style={{
-			position: 'fixed',
-			bottom: '20px',
-			right: '20px',
-			backgroundColor: '#007bff',
-			color: '#fff',
-			border: 'none',
-			borderRadius: '50%',
-			width: '60px',
-			height: '60px',
-			fontSize: '24px',
-			cursor: 'pointer',
-			boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-		}}
+		className="ufaqsw_floating-chat-button"
 		>
 		💬
 		</button>
