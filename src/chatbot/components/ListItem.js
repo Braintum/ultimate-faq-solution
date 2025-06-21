03135b747daca51024434f6f2e6276acbcd1beba
mahedihasannoman <mahedihasannoman@gmail.com>
2025-06-21 18:10:35 +0200

    Chatwindow style improved

--- src/chatbot/components/ListItem.js
+++ src/chatbot/components/ListItem.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 export const ListItem = ({ item, index, onClick, content }) => {
+	console.log( item.items );
 	return (
 		<div key={index} className="list-item" onClick={() => onClick(item)}>
-			{content}
+			<div className="title">{content}</div>
+			{item.items && (
+				<div className="description">
+					{item.items.length} FAQs
+				</div>
+			)}
 		</div>
 	);
 }
